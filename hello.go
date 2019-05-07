338e5d375a242170e3d39bc4038c7b635366e9f3
seenker <seenker@163.com>
2019-05-07 10:39:19 +0800

    0509

--- hello.go
+++ hello.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-	"fmt"
-	_"github.com/go-sql-driver/mysql"
 	"database/sql"
-	//"log"
+	"fmt"
+	"log"
+	"time"
+	_ "github.com/denisenkom/go-mssqldb"
+
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	var isdebug = true
+	var server = "localhost"
+	var port = 1433
+	var user = "sa"
+	var password = "1qaz2wsx"
+	var database = "kaoqin"
+
+	//连接字符串
+	connString := fmt.Sprintf("server=%s;port=%d;database=%s;user id=%s;password=%s",server,post,database,user,password)
+	if isdebug{
+		fmt.Println(connString)
+	}
+
+	//建立连接
+	conn, err := sql.Open("mssql",connString)
+	if err !=nil{
+		log.Fatal("Open Connection failed:",err.Error())
+	}
+	
+	defer conn.Close()
+
+
+
 }
